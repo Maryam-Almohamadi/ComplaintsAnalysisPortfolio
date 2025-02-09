88d2d0a9e247d1ed0301b96b457342b251009241
Maryam-Almohamadi <maryam.almohamedi276@outlook.com>
2025-02-09 11:23:56 +0300

    Update vectorizing.py

--- scr/vectorizing.py
+++ scr/vectorizing.py
@@ -1,6 +1,6 @@
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
-def vectorizer(text, technique): # By passing the parameters "BoW" or "TF-IDF" the function apply chosen vectorizer to text.
+def vectorizer(text, technique): # By passing the parameters "BoW" or "TF-IDF" the function apply chosen vectorization technique to text.
     vectorize = CountVectorizer(max_features=1000, stop_words='english') \
         if technique == "BoW" else TfidfVectorizer(max_features=1000, stop_words='english')
     matrix = vectorize.fit_transform(text)
